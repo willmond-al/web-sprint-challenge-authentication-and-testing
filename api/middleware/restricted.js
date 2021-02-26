a905e0df35fbd8f0304d4e0c09ac28da3a2d5991
willmondal <wbmondal@aol.com>
2021-02-26 08:57:17 -0800

    restricted js file

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken')
+const jwtSecret  = process.env.JWT_SECRET
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+
+  if(!token){
+    res.status(401).json("token required")
+  }else{
+    jwt.verify(token, jwtSecret, (err, decoded)=>{
+      if(err){
+        res.status(401).json("token invalid")
+      }else{
+        req.decodedToken = decoded
+        next();
+      }
+    })
+  }
   /*
     IMPLEMENT
 
